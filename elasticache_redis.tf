573f5e9d98db037e69745c647a8c2a138f6c12d8
jasongauvin <contact.jason.gauvin@gmail.com>
2020-03-03 17:21:09 +0100

    add elasticache_redis

---
+++ elasticache_redis.tf
@@ -0,0 +1,9 @@
+resource "aws_elasticache_cluster" "example" {
+  cluster_id           = "cluster-example"
+  engine               = "redis"
+  node_type            = "t2.micro"
+  num_cache_nodes      = 1
+  parameter_group_name = "default.redis3.2"
+  engine_version       = "3.2.10"
+  port                 = 6379
+}
